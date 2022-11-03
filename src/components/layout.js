7da7d74a5365bc496844b285acc74e16a3541cec
supports7 <57338137+supports7@users.noreply.github.com>
2022-11-04 12:38:34 +1300

    Add files via upload
    Add bootstrap styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { Container, Nav, Row, Col } from "react-bootstrap";
 import ResearchReviewNavbar from './navbar'
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Layout = ({ children }) => {
   // const rootPath = `${__PATH_PREFIX__}/`
   // const isRootPath = location.pathname === rootPath
